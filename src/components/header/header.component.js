3c67e86cd990d03b34aec16eb36858fbcd385537
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2021-01-05 16:36:07 +0100

    sign in with email/password and redux data flow in authentication

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+
+import { connect } from 'react-redux';
 import { auth } from '../../firebase/firebase.utils'; 
 import "./header.styles.scss";
 
@@ -25,10 +27,13 @@ const Header = ({ currentUser }) => (
           <Link className="option" to="/signIn">
             LOG IN
           </Link>
-      }
-      
+      }  
     </div>
   </div>
 );
 
-export default Header;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(Header);
--- src/components/sign-in/sign-in.component.js
+++ src/components/sign-in/sign-in.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -15,10 +15,16 @@ class SignIn extends React.Component {
     }
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' })
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch(error) {
+      console.error(error);
+    }
   };
 
   handleChange = event => {
@@ -54,6 +60,7 @@ class SignIn extends React.Component {
           <div className='buttons'>
             <CustomButton type='submit'>Sign In</CustomButton>
             <CustomButton 
+              type='button'
               secondary 
               onClick={signInWithGoogle}>
               Sign in with Google
