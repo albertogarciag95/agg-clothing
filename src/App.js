92c50041af605b342974f9becf7b93607489661c
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2021-01-11 12:41:54 +0100

    using selectors with reselect in cart dropdown, and checkout page

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import './App.css';
 
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import Header from "./components/header/header.component";
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 class App extends React.Component {
 
@@ -41,17 +44,26 @@ class App extends React.Component {
         <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route exact path='/shop' component={ShopPage} />
+          <Route path='/shop' component={ShopPage} />
           <Route exact path='/signIn' render={() => 
             this.props.currentUser ? <Redirect to='/' /> : <SignInAndSignUp />} />
+          <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     );
   }
 }
+// without reselect: currentUser is a string (primitive), 
+// so every state change won't actually re-render App component if currentUser remains the same value
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+// const mapStateToProps = state => ({
+//   currentUser: selectCurrentUser(state)
+// });
+
+// with reselect: 
+
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -7,9 +7,23 @@ export const selectCartItems = createSelector(
   cart => cart.cartItems
 );
 
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
-  cartItems => cartItems.reduce((accumulated, currentItem) => {
-    return accumulated + currentItem.quantity;
-  }, 0)
+  cartItems => 
+    cartItems.reduce((accumulated, currentItem) => {
+      return accumulated + currentItem.quantity;
+    }, 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems => 
+    cartItems.reduce((accumulated, currentItem) => {
+      return accumulated + (currentItem.quantity * currentItem.price);
+    }, 0)
 )
