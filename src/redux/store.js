511a45961cb956d8fc65718dfd08985ee9ec3857
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2021-01-15 09:40:58 +0100

    removing logger middleware for production build

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,7 +4,11 @@ import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if(process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares)); 
 //spread all the middlewares in individual arguments for the applyMiddleware functione
