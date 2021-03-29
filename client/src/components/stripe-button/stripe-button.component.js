8f95c44c5658c22a9a62e180028074b84bebe48e
alberto.garcia@bluetab.net <alberto.garcia@bluetab.net>
2021-03-29 12:57:04 +0200

    Separating our app into client folder, added express server and handling stripe payments

--- client/src/components/stripe-button/stripe-button.component.js
+++ client/src/components/stripe-button/stripe-button.component.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51I9DigB8ym7DUnNBzj6wBGuBBJx46R5NtqD2m4i0szN7LIU67a1fBF88gTxsyUjHQQco3Bne2YBFbkt1I5PIzZHl00XZABVHVx';
 
   const onToken = token => {
-    console.log(token);
-    alert('payment succeded');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment succesful')
+    }).catch(error => {
+      console.log('Payment error', JSON.parse(error));
+      alert('There was an issue with your payment')
+    })
   }
 
   return (
